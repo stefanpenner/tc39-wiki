8e904472e5ee9995f8e145e6060d9c2b8de3b2ea
Stefan Penner <stefan.penner@gmail.com>
2016-02-01 15:52:55 -0800

    import

---
+++ feed.php
@@ -0,0 +1,205 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="FeedCreator 1.7.1" -->
+<?xml-stylesheet href="http://wiki.ecmascript.org/lib/styles/feed.css" type="text/css"?>
+<rdf:RDF
+    xmlns="http://purl.org/rss/1.0/"
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+    xmlns:dc="http://purl.org/dc/elements/1.1/">
+    <channel rdf:about="http://wiki.ecmascript.org/feed.php">
+        <title>ES Wiki</title>
+        <description></description>
+        <link>http://wiki.ecmascript.org/</link>
+        <image rdf:resource="http://wiki.ecmascript.org/lib/images/favicon.ico" />
+       <dc:date>2015-09-09T15:11:53+01:00</dc:date>
+        <items>
+            <rdf:Seq>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=proposals:hashcodes&amp;amp;do=diff&amp;amp;1441811250"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=strawman:string_padding&amp;amp;do=diff&amp;amp;1438197652"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:specification_drafts&amp;amp;do=diff&amp;amp;1429285982"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=globalization:specification_drafts&amp;amp;do=diff&amp;amp;1427205408"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:observe_spec_changes&amp;amp;do=diff&amp;amp;1418421892"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:observe_public_api&amp;amp;do=diff&amp;amp;1418421868"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:observe_internals&amp;amp;do=diff&amp;amp;1418421851"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:observe_overview&amp;amp;do=diff&amp;amp;1418421826"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:observe_api_usage&amp;amp;do=diff&amp;amp;1418421802"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:observe&amp;amp;do=diff&amp;amp;1418421778"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:observe_updates&amp;amp;do=diff&amp;amp;1418421757"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:older_es6_drafts&amp;amp;do=diff&amp;amp;1417990761"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=test262:coreteam&amp;amp;do=diff&amp;amp;1403672804"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:modules_semantics&amp;amp;do=diff&amp;amp;1402417594"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:modules_rationale&amp;amp;do=diff&amp;amp;1402417583"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:modules_examples&amp;amp;do=diff&amp;amp;1402417569"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:module_loaders&amp;amp;do=diff&amp;amp;1402417555"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=harmony:modules&amp;amp;do=diff&amp;amp;1402417500"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=globalization:messageformatting&amp;amp;do=diff&amp;amp;1400688571"/>
+                <rdf:li rdf:resource="http://wiki.ecmascript.org/doku.php?id=strawman:data_parallelism&amp;amp;do=diff&amp;amp;1392922569"/>
+            </rdf:Seq>
+        </items>
+    </channel>
+    <image rdf:about="http://wiki.ecmascript.org/lib/images/favicon.ico">
+        <title>ES Wiki</title>
+        <link>http://wiki.ecmascript.org/</link>
+        <url>http://wiki.ecmascript.org/lib/images/favicon.ico</url>
+    </image>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=proposals:hashcodes&amp;amp;do=diff&amp;amp;1441811250">
+        <dc:format>text/html</dc:format>
+        <dc:date>2015-09-09T15:07:30+01:00</dc:date>
+        <dc:creator>Mark S. Miller (markm@173.164.252.202)</dc:creator>
+        <title>proposals:hashcodes</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=proposals:hashcodes&amp;amp;do=diff&amp;amp;1441811250</link>
+        <description> (Also see the discussion page for this proposal)   Open tickets  Ticket #52: What is the interaction of intrinsic::hashcode with the overloaded === operator?   Proposal   A global, non-deletable, non-alterable function intrinsic::hashcode(o:*):uint ...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=strawman:string_padding&amp;amp;do=diff&amp;amp;1438197652">
+        <dc:format>text/html</dc:format>
+        <dc:date>2015-07-29T19:20:52+01:00</dc:date>
+        <dc:creator>Rick Waldron (rwaldron@167.220.101.196)</dc:creator>
+        <title>strawman:string_padding</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=strawman:string_padding&amp;amp;do=diff&amp;amp;1438197652</link>
+        <description> This initial proposal was drafted by Kevin Grandon with input from Rick Waldron and Dave Herman. These proposals will be presented for inclusion in ecma-262, 7th edition. String.prototype.padLeft&amp;#40;minLength, fillStr = ' '&amp;#41;; &amp;nbsp; String.prot...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:specification_drafts&amp;amp;do=diff&amp;amp;1429285982">
+        <dc:format>text/html</dc:format>
+        <dc:date>2015-04-17T15:53:02+01:00</dc:date>
+        <dc:creator>Allen Wirfs-Brock (allen@50.38.122.171)</dc:creator>
+        <title>harmony:specification_drafts</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:specification_drafts&amp;amp;do=diff&amp;amp;1429285982</link>
+        <description> This page contains a historical record of working draft of the ES.next specification prepared by the project editor.  Errors in the current draft should be reported as bugs at bugs.ecmascript.org. Report the version you are reading, and an appropria...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=globalization:specification_drafts&amp;amp;do=diff&amp;amp;1427205408">
+        <dc:format>text/html</dc:format>
+        <dc:date>2015-03-24T13:56:48+01:00</dc:date>
+        <dc:creator>Rick Waldron (rwaldron@108.41.174.100)</dc:creator>
+        <title>globalization:specification_drafts</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=globalization:specification_drafts&amp;amp;do=diff&amp;amp;1427205408</link>
+        <description> This page contains a historical record of working drafts of the ECMAScript Internationalization API specification prepared by the project editor.  Errors in the current draft should be reported as bugs at bugs.ecmascript.org.   Release Candidate Dra...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:observe_spec_changes&amp;amp;do=diff&amp;amp;1418421892">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-12-12T22:04:52+01:00</dc:date>
+        <dc:creator>Erik Arvidsson (arv@72.14.228.137)</dc:creator>
+        <title>harmony:observe_spec_changes</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:observe_spec_changes&amp;amp;do=diff&amp;amp;1418421892</link>
+        <description>  WARNING: This page is out of date. Please see the latest draft on GitHub for up-to-date information. This page is here only as an archive.   Object.observe: Modifications to existing internal algorithms   [[ValidateAndApplyPropertyDescriptor]]   Mo...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:observe_public_api&amp;amp;do=diff&amp;amp;1418421868">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-12-12T22:04:28+01:00</dc:date>
+        <dc:creator>Erik Arvidsson (arv@72.14.228.137)</dc:creator>
+        <title>harmony:observe_public_api</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:observe_public_api&amp;amp;do=diff&amp;amp;1418421868</link>
+        <description>  WARNING: This page is out of date. Please see the latest draft on GitHub for up-to-date information. This page is here only as an archive.   Object.observe: Public API Specification   Object.observe   A new function Object.observe(O, callback, acce...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:observe_internals&amp;amp;do=diff&amp;amp;1418421851">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-12-12T22:04:11+01:00</dc:date>
+        <dc:creator>Erik Arvidsson (arv@72.14.228.137)</dc:creator>
+        <title>harmony:observe_internals</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:observe_internals&amp;amp;do=diff&amp;amp;1418421851</link>
+        <description>  WARNING: This page is out of date. Please see the latest draft on GitHub for up-to-date information. This page is here only as an archive.   Object.observe: New Internal Properties, Objects and Algorithms   [[ObserverCallbacks]]   There is now an o...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:observe_overview&amp;amp;do=diff&amp;amp;1418421826">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-12-12T22:03:46+01:00</dc:date>
+        <dc:creator>Erik Arvidsson (arv@72.14.228.137)</dc:creator>
+        <title>harmony:observe_overview</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:observe_overview&amp;amp;do=diff&amp;amp;1418421826</link>
+        <description>  WARNING: This page is out of date. Please see the latest draft on GitHub for up-to-date information. This page is here only as an archive.   Object.observe: Key Algorithms and Semantics   ChangeObserver and PendingChangeRecords   Conceptually, at t...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:observe_api_usage&amp;amp;do=diff&amp;amp;1418421802">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-12-12T22:03:22+01:00</dc:date>
+        <dc:creator>Erik Arvidsson (arv@72.14.228.137)</dc:creator>
+        <title>harmony:observe_api_usage</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:observe_api_usage&amp;amp;do=diff&amp;amp;1418421802</link>
+        <description>  WARNING: This page is out of date. Please see the latest draft on GitHub for up-to-date information. This page is here only as an archive.   Object.observe: API Usage Example   Basic var records; function observer&amp;#40;recs&amp;#41; &amp;#123;  records = re...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:observe&amp;amp;do=diff&amp;amp;1418421778">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-12-12T22:02:58+01:00</dc:date>
+        <dc:creator>Erik Arvidsson (arv@72.14.228.137)</dc:creator>
+        <title>harmony:observe</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:observe&amp;amp;do=diff&amp;amp;1418421778</link>
+        <description>  WARNING: This page is out of date. Please see the latest draft on GitHub for up-to-date information. This page is here only as an archive.   Object.observe   Updates   Updates   Problem  Declarative programming techniques provide leverage and power...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:observe_updates&amp;amp;do=diff&amp;amp;1418421757">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-12-12T22:02:37+01:00</dc:date>
+        <dc:creator>Erik Arvidsson (arv@72.14.228.137)</dc:creator>
+        <title>harmony:observe_updates</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:observe_updates&amp;amp;do=diff&amp;amp;1418421757</link>
+        <description>  WARNING: This page is out of date. Please see the latest draft on GitHub for up-to-date information. This page is here only as an archive.   Object.observe: Updates  11/14/2013:  Re-organized structure.  Added &amp;ldquo;Key Algorithms and Semantics&amp;rd...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:older_es6_drafts&amp;amp;do=diff&amp;amp;1417990761">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-12-07T22:19:21+01:00</dc:date>
+        <dc:creator>Allen Wirfs-Brock (allen@50.126.109.173)</dc:creator>
+        <title>harmony:older_es6_drafts - created</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:older_es6_drafts&amp;amp;do=diff&amp;amp;1417990761</link>
+        <description> doc pdf with change markup only for this revisionpdf with cumulative change markup pdf without markup  Changes marked as &amp;ldquo;Rev 19&amp;ldquo;. Cumulative change markup is relative to Rev 17, August 23, 2013    Changes include:  Better internal cross...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=test262:coreteam&amp;amp;do=diff&amp;amp;1403672804">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-06-25T05:06:44+01:00</dc:date>
+        <dc:creator>Norbert Lindenberg (norbert@70.36.142.15)</dc:creator>
+        <title>test262:coreteam - Removed myself from Test262 core team.</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=test262:coreteam&amp;amp;do=diff&amp;amp;1403672804</link>
+        <description> The core team has access to the Mercurial repository and ability to update the website as required.  Core Team  Brian Terlson (Microsoft, Inc.)  Brent Baker (Adobe Systems, Inc.)    ...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:modules_semantics&amp;amp;do=diff&amp;amp;1402417594">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-06-10T16:26:34+01:00</dc:date>
+        <dc:creator>Dave Herman (dherman@67.180.182.145)</dc:creator>
+        <title>harmony:modules_semantics</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:modules_semantics&amp;amp;do=diff&amp;amp;1402417594</link>
+        <description>  WARNING: This page is out of date. Please see the ES6 specification drafts for up-to-date information. This page is here only as an archive.   Modules Semantics  Each inline module declaration introduces a fresh scope. The module name is bound in t...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:modules_rationale&amp;amp;do=diff&amp;amp;1402417583">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-06-10T16:26:23+01:00</dc:date>
+        <dc:creator>Dave Herman (dherman@67.180.182.145)</dc:creator>
+        <title>harmony:modules_rationale</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:modules_rationale&amp;amp;do=diff&amp;amp;1402417583</link>
+        <description>  WARNING: This page is out of date. Please see the ES6 specification drafts for up-to-date information. This page is here only as an archive.   Motivations   Core module system  Standardized mechanism for creating libraries.  In-language loading of ...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:modules_examples&amp;amp;do=diff&amp;amp;1402417569">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-06-10T16:26:09+01:00</dc:date>
+        <dc:creator>Dave Herman (dherman@67.180.182.145)</dc:creator>
+        <title>harmony:modules_examples</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:modules_examples&amp;amp;do=diff&amp;amp;1402417569</link>
+        <description>  WARNING: This page is out of date. Please see the ES6 specification drafts for up-to-date information. This page is here only as an archive.   Modules: Examples   A simple module module 'math' &amp;#123;   export function sum&amp;#40;x, y&amp;#41; &amp;#123;     r...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:module_loaders&amp;amp;do=diff&amp;amp;1402417555">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-06-10T16:25:55+01:00</dc:date>
+        <dc:creator>Dave Herman (dherman@67.180.182.145)</dc:creator>
+        <title>harmony:module_loaders</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:module_loaders&amp;amp;do=diff&amp;amp;1402417555</link>
+        <description>  WARNING: This page is out of date. Please see the ES6 specification drafts for up-to-date information. This page is here only as an archive.   Module loaders   This page describes a dynamic API for loading modules in controlled and selectively isol...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=harmony:modules&amp;amp;do=diff&amp;amp;1402417500">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-06-10T16:25:00+01:00</dc:date>
+        <dc:creator>Dave Herman (dherman@67.180.182.145)</dc:creator>
+        <title>harmony:modules</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=harmony:modules&amp;amp;do=diff&amp;amp;1402417500</link>
+        <description>  WARNING: This page is out of date. Please see the ES6 specification drafts for up-to-date information. This page is here only as an archive.   Modules   This proposal describes a module system for ECMAScript. It describes the semantics of the core ...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=globalization:messageformatting&amp;amp;do=diff&amp;amp;1400688571">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-05-21T16:09:31+01:00</dc:date>
+        <dc:creator>Zbigniew Braniecki (zbraniecki@50.184.187.142)</dc:creator>
+        <title>globalization:messageformatting</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=globalization:messageformatting&amp;amp;do=diff&amp;amp;1400688571</link>
+        <description> The only standard way to build/format a string in JavaScript is to concatenate parts of the string. This approach is not readable and it introduces possible localization errors, thus most of JavaScript libraries (jQuery, Dojo, YUI, Closure) implemen...</description>
+    </item>
+    <item rdf:about="http://wiki.ecmascript.org/doku.php?id=strawman:data_parallelism&amp;amp;do=diff&amp;amp;1392922569">
+        <dc:format>text/html</dc:format>
+        <dc:date>2014-02-20T18:56:09+01:00</dc:date>
+        <dc:creator>Rick Hudson (rick.hudson@199.116.173.42)</dc:creator>
+        <title>strawman:data_parallelism</title>
+        <link>http://wiki.ecmascript.org/doku.php?id=strawman:data_parallelism&amp;amp;do=diff&amp;amp;1392922569</link>
+        <description> This proposal describes a gentle extension to EcmaScript that will enable access to the parallelism found in modern processors.   Goals  The goal of this proposal is to enable data-parallelism in web applications. Browser applications and in particu...</description>
+    </item>
+</rdf:RDF>
